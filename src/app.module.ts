6c3700189d58ef0d465c079d720bbcb5556b0e11
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-04-30 23:41:29 -0300

    feat: create resources cars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
