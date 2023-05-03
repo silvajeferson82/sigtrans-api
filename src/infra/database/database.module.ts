59132eefbc4c98b08e56a2b0006469c66d06b8fe
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-05-03 16:32:33 -0300

    feat: include CarRepository and CarRepositoryPrisma

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { CarRepository } from '../../application/repositories/car-repository';
+import { CarRepositoryPrisma } from './car.repository-prisma';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: CarRepository,
+      useClass: CarRepositoryPrisma,
+    },
+  ],
+  exports: [
+    {
+      provide: CarRepository,
+      useClass: CarRepositoryPrisma,
+    },
+  ],
 })
 export class DatabaseModule {}
