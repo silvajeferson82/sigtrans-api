68c66abe9ec43eb528b59b03656e40c3924d8162
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-05-03 16:34:39 -0300

    feat: include CreateCarUseCase

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
-import { CarsController } from './controllers/cars.controller';
+import { CarsController } from './controllers/car-constrollers/cars.controller';
+import { CreateCarUseCase } from '../../application/useCases';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [CarsController],
-  providers: [],//useCases
+  providers: [CreateCarUseCase], //useCases
 })
 export class HttpModule {}
