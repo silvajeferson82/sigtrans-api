bd0903b4657bf51efb5871652c1a3b5d20734e33
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-05-03 01:37:29 -0300

    feat: car repotitory

---
+++ src/application/repositories/car-repository.ts
@@ -0,0 +1,9 @@
+import { CarEntity } from '../entities/car.entity';
+
+export abstract class CarRepository {
+  abstract create(car: CarEntity): Promise<CarEntity>;
+  abstract findAll(): Promise<CarEntity[]>;
+  abstract findOne(id: string): Promise<CarEntity>;
+  abstract update(id: string, car: Car): Promise<CarEntity>;
+  abstract remove(id: string): Promise<CarEntity>;
+}
