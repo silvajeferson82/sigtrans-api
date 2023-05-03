36decda3c8ca7bc1815957d0e1598a7d6721334d
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-05-02 22:36:05 -0300

    config database  prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { DatabaseModule } from './core/infra/database/database.module';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [PrismaModule, CarsModule],
+  imports: [DatabaseModule, CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
