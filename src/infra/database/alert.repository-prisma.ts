e625f1683b39cfd9d7a093b57791a2f0cf238817
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-05-04 04:21:05 -0300

    feat: create functions

--- src/infra/database/alert.repository-prisma.ts
+++ src/infra/database/alert.repository-prisma.ts
@@ -1,25 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { AlertEntity } from 'src/application/entities/alert.entity';
-import { AlertRepository } from 'src/application/repositories/alert-repository';
+import { AlertEntity } from '../../domain/entities/alert.entity';
+import { AlertRepository } from '../../domain/repositories/alert-repository';
 import { PrismaService } from './prisma/prisma.service';
-
 
 @Injectable()
 export class AlertRepositoryPrisma implements AlertRepository {
   constructor(private readonly prisma: PrismaService) {}
-  create(data: IRequestAlert): Promise<AlertEntity> {
-    throw new Error('Method not implemented.');
+  async create(data: IRequestAlert): Promise<AlertEntity> {
+    const result = await this.prisma.alert.create({
+      data,
+    });
+    return result;
   }
   findAll(): Promise<AlertEntity[]> {
-    throw new Error('Method not implemented.');
+    const result = this.prisma.alert.findMany();
+    return result;
   }
   findOne(id: string): Promise<AlertEntity> {
-    throw new Error('Method not implemented.');
+    const result = this.prisma.alert.findUnique({
+      where: { id },
+    });
+    return result;
   }
   update(id: string, alert: IRequestAlert): Promise<AlertEntity> {
-    throw new Error('Method not implemented.');
+    const result = this.prisma.alert.update({
+      where: { id },
+      data: { ...alert },
+    });
+    return result;
   }
   remove(id: string): Promise<AlertEntity> {
-    throw new Error('Method not implemented.');
+    const result = this.prisma.alert.delete({
+      where: { id },
+    });
+    return result;
   }
 }
